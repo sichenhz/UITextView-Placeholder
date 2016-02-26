e1fd3570d3149979b9bcbd78e3b9dfd3c46d5b9d
sichenwang <52870104@qq.com>
2016-02-26 17:41:23 +0800

    <修复> 当frame font等属性改变后，placeholder的label frame无法更新的bug

--- UITextView-Placeholder/UITextView-Placeholder/UITextView+Placeholder.m
+++ UITextView-Placeholder/UITextView-Placeholder/UITextView+Placeholder.m
@@ -19,8 +19,23 @@ static CGFloat kTopMargin = 8;
                                    class_getInstanceMethod(self.class, @selector(swizzledDealloc)));
 }
 
++ (NSArray *)observingKeys {
+    return @[@"bounds", @"font", @"frame", @"text"];
+}
+
 - (void)swizzledDealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
+    UILabel *label = objc_getAssociatedObject(self, @selector(label));
+    if (label) {
+        for (NSString *key in self.class.observingKeys) {
+            @try {
+                [self removeObserver:self forKeyPath:key];
+            }
+            @catch (NSException *exception) {
+                
+            }
+        }
+    }
     [self swizzledDealloc];
 }
 
@@ -31,17 +46,30 @@ static CGFloat kTopMargin = 8;
     }
 }
 
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context {
+    [self layoutLabel];
+}
+
+- (void)layoutLabel {
+    CGSize rectangleSize = CGSizeMake(self.frame.size.width - kLeftMargin * 2, MAXFLOAT);
+    CGSize size = [self.placeholder boundingRectWithSize:rectangleSize
+                                                 options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading | NSStringDrawingTruncatesLastVisibleLine
+                                              attributes:@{NSFontAttributeName:self.label.font}
+                                                 context:nil].size;
+    self.label.frame = CGRectMake(kLeftMargin, kTopMargin, size.width, size.height);
+}
+
 - (void)setPlaceholder:(NSString *)placeholder {
     if (self.placeholder != placeholder) {
         objc_setAssociatedObject(self, @selector(placeholder), placeholder, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
         self.label.text = placeholder;
-        CGSize rectangleSize = CGSizeMake(self.frame.size.width - kLeftMargin * 2, MAXFLOAT);
-        CGSize size = [placeholder boundingRectWithSize:rectangleSize
-                                                options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading | NSStringDrawingTruncatesLastVisibleLine
-                                             attributes:@{NSFontAttributeName:self.label.font}
-                                                context:nil].size;
-        self.label.frame = CGRectMake(kLeftMargin, kTopMargin, size.width, size.height);
         [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textDidChange:) name:UITextViewTextDidChangeNotification object:nil];
+        for (NSString *key in self.class.observingKeys) {
+            [self addObserver:self forKeyPath:key options:NSKeyValueObservingOptionNew context:nil];
+        }
     }
 }
 
@@ -67,6 +95,7 @@ static CGFloat kTopMargin = 8;
         label.font = self.font ? : [UIFont systemFontOfSize:14];
         label.textColor = self.placeholderColor ? : [UIColor colorWithRed:204/255.0 green:204/255.0 blue:204/255.0 alpha:1];
         label.textAlignment = NSTextAlignmentLeft;
+        label.userInteractionEnabled = NO;
         label.numberOfLines = 0;
         [self addSubview:label];
         objc_setAssociatedObject(self, @selector(label), label, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
